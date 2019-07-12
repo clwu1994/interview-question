6319a5307236b1504a53ebc90afa5420ef152404
clwu1994 <1445860318@qq.com>
2019-07-12 18:12:22 +0800

    双向数据绑定有缺陷

--- mvvm/mvvm.js
+++ mvvm/mvvm.js
@@ -1,3 +1,13 @@
+function MVue(options) {
+  this.$el = options.el;
+  this.$data = options.data;
+  // 数据监听
+  obverser(this.$data);
+  // 模板编译
+  let elem = document.querySelector(this.$el);
+  elem.appendChild(nodeToFragment(elem, this));
+}
+
 // vue进行编译时，将挂载目标的所有子节点劫持到DocumentFragment中，
 // 经过一份解析等处理后，再将DocumentFragment整体挂载到目标节点上。
 function nodeToFragment(node, vm) {
@@ -29,7 +39,8 @@ function compile(node, vm) {
           vm.$data[name] = e.target.value;
         });
         // 初始化数据绑定
-        node.value = vm.$data[name];
+        // node.value = vm.$data[name];
+        new Watcher(vm, node, name);
         // 移除v-model 属性
         node.removeAttribute("v-model");
       }
@@ -40,22 +51,15 @@ function compile(node, vm) {
     if (reg.test(node.nodeValue)) {
       var name = RegExp.$1 && RegExp.$1.trim();
       // 绑定数据到文本节点中
-      node.nodeValue = node.nodeValue.replace(
-        new RegExp("\\{\\{\\s*(" + name + ")\\s*\\}\\}"),
-        vm.$data[name]
-      );
+      // node.nodeValue = node.nodeValue.replace(
+      //   new RegExp("\\{\\{\\s*(" + name + ")\\s*\\}\\}"),
+      //   vm.$data[name]
+      // );
+      new Watcher(vm, node, name);
     }
   }
 }
 
-function MVue(options) {
-  this.$el = options.el;
-  this.$data = options.data;
-
-  // 模板编译
-  let elem = document.querySelector(this.$el);
-  elem.appendChild(nodeToFragment(elem, this));
-}
 // 实现model=>view 发布者-订阅者模式
 function Dep() {
   this.subs = [];
@@ -71,3 +75,76 @@ Dep.prototype = {
     });
   }
 };
+
+// 实现订阅者
+function Watcher(vm, node, name) {
+  // 全局的、唯一
+  Dep.target = this;
+  this.node = node;
+  this.name = name;
+  this.vm = vm;
+  this.index = node.nodeType;
+  this.update();
+  Dep.target = null;
+}
+Watcher.prototype = {
+  update() {
+    this.get();
+    let _name;
+    if (this.index === 1) {
+      _name = this.name;
+    } else {
+      _name = this.value;
+    }
+    if (this.node.nodeName === "INPUT") {
+      this.node.value = this.value;
+    } else {
+      // this.node.nodeValue = this.value;
+      this.node.nodeValue = this.node.nodeValue.replace(
+        new RegExp("\\{?\\{?\\s*(" + _name + ")\\s*\\}?\\}?"),
+        this.value
+      );
+    }
+    ++this.index;
+  },
+  get() {
+    this.value = this.vm.$data[this.name];
+  }
+};
+
+// 实现一个obverser给data中每个属性添加一个主题对象
+// 遍历data中的所有属性,包括子属性对象的属性
+function obverser(obj) {
+  Object.keys(obj).forEach(key => {
+    if (obj[key].constructor === "Object") {
+      obverser(obj[key]);
+    }
+    defineReactive(obj, key);
+  });
+}
+
+// 使用Object.definePeoperty()来监听属性变动，给属性添加setter和getter
+function defineReactive(obj, key) {
+  var _value = obj[key];
+  // new一个主题对象
+  var dep = new Dep();
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    set(newVal) {
+      if ((_value === newVal)) {
+        return;
+      }
+      _value = newVal;
+      // 作为发布者发出通知给主题对象
+      dep.notify();
+    },
+    get() {
+      // 如果订阅者存在，添加到主题对象中
+      if (Dep.target) {
+        dep.addSub(Dep.target);
+      }
+      return _value;
+    }
+  });
+}
