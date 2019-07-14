3280ef6709323fa733466c6cac38f16461745f1d
clwu1994 <1445860318@qq.com>
2019-07-14 11:15:01 +0800

    用JS对象模拟DOM树

---
+++ virtual-dom/index.js
@@ -0,0 +1,29 @@
+/**
+ * <ul id='list'>
+ * <li class='item'>Item 1</li>
+ * <li class='item'>Item 2</li>
+ * <li class='item'>Item 3</li>
+ * </ul>
+ */
+
+// var element = {
+//   tagName: 'ul', // 节点标签名
+//   props: { // DOM的属性，用一个对象存储键值对
+//     id: 'list'
+//   },
+//   children: [ // 该节点的子节点
+//     {tagName: 'li', props: {class: 'item'}, children: ["Item 1"]},
+//     {tagName: 'li', props: {class: 'item'}, children: ["Item 2"]},
+//     {tagName: 'li', props: {class: 'item'}, children: ["Item 3"]},
+//   ]
+// }
+
+var el = require("./element");
+var ul = el("ul", { id: "list" }, [
+  el("li", { class: "item" }, ["Item 1"]),
+  el("li", { class: "item" }, ["Item 2"]),
+  el("li", { class: "item" }, ["Item 3"])
+]);
+
+var ulRoot = ul.render();
+document.body.appendChild(ulRoot);
